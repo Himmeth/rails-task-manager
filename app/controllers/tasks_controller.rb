6611726b250ecc93a5944351dd144d2be73cefda
leo marshall <tgbrtmarshall@gmail.com>
2018-05-15 14:35:01 -0400

    finish to do list, check

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,55 @@
 class TasksController < ApplicationController
+  #before_action :task_params, only: [:create, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.all.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task_params
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+    @task = Task.all.find(params[:id])
+  end
+
+  def update
+     @task = Task.all.find(params[:id])
+
+     if params[:completed?] == 'yes'
+        @task.completed = true
+     end
+
+     @task.update(task_params)
+     redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @Task = Task.all.find(params[:id])
+    @Task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
+
+
+# As a user, I can list tasks
+# As a user, I can view the details of a task
+# As a user, I can add a new task
+# As a user, I can edit a task (mark as completed / update title & details)
+# As a user, I can remove a task
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  get 'new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  get 'edit/:id', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+
+  get 'tasks/destroy/:id', to: 'tasks#destroy'
 end
+
+# As a user, I can list tasks
+# As a user, I can view the details of a task
+# As a user, I can add a new task
+# As a user, I can edit a task (mark as completed / update title & details)
+# As a user, I can remove a task
